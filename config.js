7aa9f9ceb0ccdab11e2f49145e7fd1c70d3a69b3
Flame Master <2jhairell@gmail.com>
2021-02-23 21:15:57 +0000

    e

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
-exports.Prefix = `-`;//your prefix for bot
-exports.Token = ``;//your token 
-exports.Color = `#2194ff`;//color   of embed
+exports.Prefix = `-`; //your prefix for bot
+exports.Token = `ODA0MTAyNjE2NzA3MTA0ODI5.YBHc6w.4Lv9rfX-pMK9Om_MzabLwT9k71c`; //your token 
+exports.Color = `#2194ff`; //color   of embed
+exports.Admin = "305787316298055702",
+                "607307474092949514";
